01475bbecb01add1fb9375029b149a7bd4488f3f
httpcode <blockchaindevweb@gmail.com>
2022-07-26 15:51:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Get Started
+
+Install the dependencies:  
+```text
+npm install
+```
+
+Create an environnement .env file with your globals environnements and completed encryption key words. 
+
+Choice your 'production' or 'development' environnement in the NODE_ENV global variable for realize your encrypted file.
+
+Create a encrypted-prod.json and encrypted-dev.json file and added the global variables env witch you want to encrypt.
+
+Run the script for encrypt yours globals environnements in encrypted.js file.
+```text
+node index.js
+```
+
+Copy the data of the encrypted.js file into your frontend in a file with a service-account.enc.js, a service-account-local.enc.js or another name file.
---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: './.env' });
+const fs = require('fs');
+
+const { NODE_ENV, ENCRYPTION_KEY_WORDS, ENCRYPTION_KEY_WORDS_LOCAL } = process.env;
+
+const encryptionKeyWords = NODE_ENV === 'production' ? ENCRYPTION_KEY_WORDS : ENCRYPTION_KEY_WORDS_LOCAL;
+const encryptedFile = NODE_ENV === 'production' ? 'encrypted-prod.json' : 'encrypted-dev.json';
+
+let rawdata = fs.readFileSync(encryptedFile);
+let globalEnvJSON = JSON.parse(rawdata);
+
+const encryptDatas = crypto.AES.encrypt(JSON.stringify(globalEnvJSON), encryptionKeyWords).toString();
+
+fs.writeFile('encrypted.js', `module.exports = { encrypted: "${encryptDatas}" };`, err => {
+  if (err) return console.log(err);
+});
