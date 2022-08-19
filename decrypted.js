2ca794eeaaeb55922d43b2b6f478f33b1cfd2453
httpcode <blockchaindevweb@gmail.com>
2022-08-19 14:21:51 +0200

    hotfix: refacto code with env global enc only for production

--- decrypted.js
+++ decrypted.js
@@ -4,20 +4,16 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const main = async () => {
-  const { NODE_ENV, ENCRYPTION_KEY_WORD, ENCRYPTION_KEY_WORD_LOCAL } = process.env;
-  
-  const encryptionKeyWords = NODE_ENV === 'production'
-    ? ENCRYPTION_KEY_WORD
-    : ENCRYPTION_KEY_WORD_LOCAL;
+  const { ENCRYPTION_KEY_WORD } = process.env;
 
-    if (typeof encryptionKeyWords === 'undefined') {
+    if (typeof ENCRYPTION_KEY_WORD === 'undefined') {
         console.error('Missing encryption key');
 
         process.exit(2);
     }
 
     const encrypted = readFileSync('./encrypted.txt');
-    const decipher = crypto.AES.decrypt(encrypted.toString(), encryptionKeyWords);
+    const decipher = crypto.AES.decrypt(encrypted.toString(), ENCRYPTION_KEY_WORD);
     let decrypted = decipher.toString(crypto.enc.Utf8);
 
     console.log(decrypted)
--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-require('dotenv').config({ path: './.env' });
 const fs = require('fs');
 const crypto = require('crypto-js');
+const dotenv = require('dotenv');
+dotenv.config()
 
-const { NODE_ENV, ENCRYPTION_KEY_WORDS, ENCRYPTION_KEY_WORDS_LOCAL } = process.env;
+const { ENCRYPTION_KEY_WORD } = process.env;
 
-const encryptionKeyWords = NODE_ENV === 'production'
-  ? ENCRYPTION_KEY_WORDS
-  : ENCRYPTION_KEY_WORDS_LOCAL;
-
-const encryptedFile = NODE_ENV === 'production'
-  ? 'encrypted-prod.json'
-  : 'encrypted-dev.json';
+const encryptedFile = 'encrypted.json';
 
 let rawdata = fs.readFileSync(encryptedFile);
 let globalEnvJSON = JSON.parse(rawdata);
 
-const encryptDatas = crypto.AES.encrypt(JSON.stringify(globalEnvJSON), encryptionKeyWords).toString();
+const encryptDatas = crypto.AES.encrypt(JSON.stringify(globalEnvJSON), ENCRYPTION_KEY_WORD).toString();
 
 fs.writeFile('encrypted.txt', encryptDatas, err => {
   if (err) return console.log(err);
